23ca6be6ad9acfa6c8da861cdc2d25120f4a3b21
goldpips <skullcrushur12@gmail.com>
2025-12-25 00:05:40 +0530

    Delete app directory

--- app/app/locator/page.js
+++
@@ -1,8 +0,0 @@
-export default function Locator(){
-  return(
-    <div className="p-6">
-      <h1 className="text-xl font-bold">Store Locator</h1>
-      <p className="mt-2">Google Maps UI will be added in Phase 3 with live pharmacies.</p>
-    </div>
-  )
-}
